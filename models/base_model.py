0ff4d73cd419612003707198ff669904b8c1a59e
hamza-25 <me677499@gmail.com>
2023-10-10 10:59:45 +0000

    update __init__

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python3
 import uuid
-import datetime
+from datetime import datetime
 """representation of BaseModel"""
 
 
 class BaseModel():
     """Base Model Class"""
 
-    def __init__(self, my_number="", name=""):
+    my_number = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
         """init BaseModel initializing
         """
-        self.my_number = my_number
-        self.name = name
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updted_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at":
+                        dt_fmt = "%Y-%m-%dT%H:%M:%S.%f"
+                        self.__dict__[key] = datetime.strptime(value, dt_fmt)
+                    elif key == "updated_at":
+                        self.__dict__[key] = datetime.strptime(value, dt_fmt)
+                    else:
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updted_at = datetime.now()
 
     def save(self):
         """save function that update updted_at attrb
         """
-        self.updted_at = datetime.datetime.now()
+        self.updted_at = datetime.now()
 
     def to_dict(self):
         """get dict of attrb object
