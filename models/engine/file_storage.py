93467d3efce6b20f48eb2d15c3589c65aa838f37
hamza-25 <me677499@gmail.com>
2023-10-11 23:16:38 +0000

    fix moduleNotFound

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import json
 import os
-from models.base_model import BaseModel
 
 class FileStorage:
-    __file_path = "/home/vagrant/AirBnB_clone/file.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -15,11 +14,20 @@ class FileStorage:
         FileStorage.__objects[class_name] = obj
     
     def save(self):
+        data = {}
+        for key, value in self.__objects.items():
+            data[key] = value.to_dict()
         with open(FileStorage.__file_path, "w") as file:
-            json.dump(FileStorage.__objects, file)
+            json.dump(data, file)
     
     def reload(self):
-        if os.path.exists(FileStorage.__file_path) and os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as file:
-                FileStorage.__objects = json.load(file)
-
+        #if os.path.exists(FileStorage.__file_path) and os.path.isfile(FileStorage.__file_path):
+            #with open(FileStorage.__file_path, "r") as file:
+                #FileStorage.__objects = json.load(file)
+        if os.path.isfile(FileStorage.__file_path):
+            try:
+                with open(FileStorage.__file_path, "r") as file:
+                    data = json.load(file)
+                    FileStorage.__objects = data
+            except (FileNotFoundError, json.JSONDecodeError) as e:
+                return
