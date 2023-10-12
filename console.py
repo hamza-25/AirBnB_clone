a8a3034fd1ae768d2eaa2098a374c9eb8332bf24
hamza-25 <me677499@gmail.com>
2023-10-12 21:36:29 +0000

    create method create

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Defines the HBnB console."""
 
 import cmd
+import sys
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter.
@@ -10,18 +11,29 @@ class HBNBCommand(cmd.Cmd):
         prompt (str): The command prompt.
     """
 
+    __class = ["BaseModel", "FileStorage"]
     prompt = "(hbnb) "
 
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
         pass
+    
     def do_quit(self, arg):
         """Quits the HBnB console."""
         return True
+    
     def do_EOF(self, arg):
         """Exits the HBnB console."""
         return True
-    
+
+    def do_create(self, args):
+        words = args.split()
+        if len(words) == 0:
+            print("** class name missing **")
+        elif words[0] not in self.__class:
+            print("** class doesn't exist **")
+        else:
+            pass
 
 
 if __name__ == "__main__":
