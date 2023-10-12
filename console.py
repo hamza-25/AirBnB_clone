b2361df4f7dba34ceb9e8be9f4a5004a3b3d65e7
hamza-25 <me677499@gmail.com>
2023-10-12 23:00:39 +0000

    add create and show method

--- console.py
+++ console.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines the HBnB console."""
-
 import cmd
 import sys
+from models.base_model import BaseModel
+import json
 
 class HBNBCommand(cmd.Cmd):
     """Defines the HolbertonBnB command interpreter.
@@ -11,6 +12,7 @@ class HBNBCommand(cmd.Cmd):
         prompt (str): The command prompt.
     """
 
+
     __class = ["BaseModel", "FileStorage"]
     prompt = "(hbnb) "
 
@@ -18,11 +20,11 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing upon receiving an empty line."""
         pass
     
-    def do_quit(self, arg):
+    def do_quit(self, args):
         """Quits the HBnB console."""
         return True
     
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
         """Exits the HBnB console."""
         return True
 
@@ -33,8 +35,36 @@ class HBNBCommand(cmd.Cmd):
         elif words[0] not in self.__class:
             print("** class doesn't exist **")
         else:
-            pass
+            obj = eval(args)()
+            obj.save()
+            print(obj.id)
+    
+    def do_show(self, args):
+        """show method"""
+        words = args.split()
+        if not args:
+            print("** class name missing **")
+        elif  words[0] not in self.__class:
+            print("** class doesn't exist **")
+        elif len(words) == 1:
+            print("** instance id missing **")
+        elif len(words) == 2:
+            with open("file.json") as file:
+                data = json.load(file)
+                there_is_match = False
+                for key in data.keys():
+                    k_s = key.split('.')
+                    if words[0] == k_s[0] and words[1] == k_s[1]:
+                        there_is_match = True
+                if not there_is_match:
+                    print("** no instance found **")
+                    
+
 
 
 if __name__ == "__main__":
-    HBNBCommand().cmdloop()
+    if not sys.stdin.isatty():
+        for line in sys.stdin:
+            HBNBCommand().onecmd(line.strip())
+    else:
+        HBNBCommand().cmdloop()
