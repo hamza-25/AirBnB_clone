d186ee0ca9c7b76a70ca7e0a0f29a2f20a8fc972
hamza-25 <me677499@gmail.com>
2023-10-13 15:08:09 +0000

    add destroy method ann all

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 import sys
 from models.base_model import BaseModel
+#from models.engine.file_storage import FileStorage
 import json
 
 class HBNBCommand(cmd.Cmd):
@@ -56,9 +57,64 @@ class HBNBCommand(cmd.Cmd):
                     k_s = key.split('.')
                     if words[0] == k_s[0] and words[1] == k_s[1]:
                         there_is_match = True
+                        class_name = data[key]["__class__"]
+                        del data[key]["__class__"]
+                        cls_obj = globals().get(class_name)
+                        if cls_obj:
+                            ob = cls_obj(**data[key])
+                            print(ob)
                 if not there_is_match:
                     print("** no instance found **")
-                    
+    def do_destroy(self, args):
+        """destroy method"""
+        words = args.split()
+        if not args:
+            print("** class name missing **")
+        elif  words[0] not in self.__class:
+            print("** class doesn't exist **")
+        elif len(words) == 1:
+            print("** instance id missing **")
+        elif len(words) == 2:
+            with open("file.json") as file:
+                data = json.load(file)
+                there_is_match = False
+                for key in data.keys():
+                    k_s = key.split('.')
+                    if words[0] == k_s[0] and words[1] == k_s[1]:
+                        del data[key]
+                        with open("file.json", "w") as file:
+                            json.dump(data, file)
+                        return
+                if not there_is_match:
+                    print("** no instance found **")
+
+    def do_all(self, args):
+        """all method"""
+        words = args.split()
+        if not args:
+             with open("file.json") as file:
+                data = json.load(file)
+                for key in data.keys():
+                    class_name = data[key]["__class__"]
+                    del data[key]["__class__"]
+                    cls_obj = globals().get(class_name)
+                    if cls_obj:
+                        ob = cls_obj(**data[key])
+                        print(ob)
+        elif words[0] not in self.__class and len(words) > 0:
+            print("** class doesn't exist **")
+        elif words[0] in self.__class and len(words) > 0:
+            with open("file.json") as file:
+                data = json.load(file)
+                for key in data.keys():
+                    k_s = key.split('.')
+                    if words[0] == k_s[0]:
+                        class_name = data[key]["__class__"]
+                        del data[key]["__class__"]
+                        cls_obj = globals().get(class_name)
+                        if cls_obj:
+                            ob = cls_obj(**data[key])
+                            print(ob)
 
 
 
