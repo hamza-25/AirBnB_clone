bfa588dfda91c5e69ba5f100f80f4d7e942224c1
hamza-25 <me677499@gmail.com>
2023-10-14 14:15:52 +0000

    fix pycodestyle for folder unittes

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+"""
 from models.base_model import BaseModel
 import unittest
 
 
 class BaseModelTest(unittest.TestCase):
-    
+    """REPRESENTATION  Unittests
+        methods:
+            test_basic_creation
+            test_check_instance
+            test_id_not_equal
+    """
     def test_basic_creation(self):
         obj1 = BaseModel()
-        not(self.assertIsNone(print(obj1), "test_basic_creation"))
+        not (self.assertIsNone(print(obj1), "test_basic_creation"))
 
     def test_check_instance(self):
         obj1 = BaseModel()
@@ -17,6 +24,7 @@ class BaseModelTest(unittest.TestCase):
         obj1 = BaseModel()
         obj2 = BaseModel()
         self.assertIsNot(obj1.id, obj2.id, "must be not equal")
+
 
 if __name__ == "__main__":
     unittest.main()
